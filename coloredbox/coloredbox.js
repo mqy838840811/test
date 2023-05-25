a18d4c22cf5ee5befba86c2475912f861a32a431
mqy838840811 <838840811@qq.com>
2023-05-25 10:42:43 +0800

    Update coloredbox.js

--- coloredbox/coloredbox.js
+++ coloredbox/coloredbox.js
@@ -70,9 +70,47 @@ var context =this;
 			this._props = { ...this._props, ...changedProperties };
 		}
 		
-		get result() {
-			return context.result;
-		}
+		get (url, data, dataType) {
+		      //获取营业厅
+			var oParams = {    
+				"appId":"app_000001",
+				"processId": "processId",
+				"processInstId":"processInstId",
+				"taskId": "taskId",
+				"title": "创建待办", 
+				"description": "创建待办描述",
+				"dueTime": "2022-04-08 10:23:45",
+				"executorIds": ["10101186"],
+				"participantIds":["10101186"],
+				"appUrl":"https://www.baidu.com",
+				"pcUrl":"https://www.bing.com",
+				"onlyShowExecutor":false,
+				"operatorId": "10101186"
+			};
+			jQuery.ajax({
+				type: "POST",
+				contentType: "application/json",
+				headers: {
+					"X-GW-AccessKey": 'OGJDK2bmuRQrijXILVop0p8YRxIDFNbF',
+					"Content-Type": 'application/json'
+					},
+				url: 'https://rdfa-gateway.uat.ennew.com/icome-contact/todo/create',
+				dataType: "json",
+				async: false,
+				data: JSON.stringify(oParams),
+				success: function (data) {
+					console.log(data);
+					context.result = data;
+					var eventonRequest = new Event("onRequest");
+					context.dispatchEvent(eventonRequest);
+					
+				},
+				error: function (XMLHttpRequest, textStatus, errorThrown) {
+					console.log(textStatus);
+				},
+			});
+		      return context.result;
+		   }
 		
 		onCustomWidgetAfterUpdate(changedProperties) {
 			if ("color" in changedProperties) {
